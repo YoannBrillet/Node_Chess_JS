9ee383a6a2967341f041ea95cadaadada05a75e6
antoinelemarie <antoine.lemarie@ynov.com>
2017-05-19 08:43:48 +0200

    chat
    j’ai pas test encore

--- Node_Chess_JS/start.js
+++ Node_Chess_JS/start.js
@@ -4,34 +4,37 @@ const SocketServer = require('socket.io');
 var joueurs = [];
 
 const httpServer = http.createServer((request, response) => {
-    console.log('REQUEST');
-    fs.readFile(__dirname + '/index.html', (err, data) => {
-        response.end(data);
-    });
+  console.log('REQUEST');
+  fs.readFile(__dirname + '/index.html', (err, data) => {
+    response.end(data);
+  });
 });
 
 const io = new SocketServer(httpServer);
 io.on('connection', (socket) => {
-    console.log('SOCKET CONNECTION : ' + socket.id);
-    joueurs.push(socket);
-    socket.on('pseudo', (data) =>{
-        console.log('MESSAGE RECEIVED : ' + data.text + ' FROM ' + socket.id);
+  console.log('SOCKET CONNECTION : ' + socket.id);
+  joueurs.push(socket);
+  socket.on('pseudo', (data) => {
+    console.log('MESSAGE RECEIVED : ' + data.text + ' FROM ' + socket.id);
 
-        setTimeout(() => {
-            socket.emit('reponse', {text: 'Bien recu'});
-        }, 2000)
+    setTimeout(() => {
+      socket.emit('reponse', {
+        text: 'Bien recu'
+      });
+    }, 2000)
 
-    });
+  });
 
-    socket.on('disconnect', (data) => {
-        for (var i in joueurs) {
-            if (joueurs[i].id == socket.id){
-                joueurs.splice(i, 1);
-                break;
-            }
-        }
-    })
-1});
+  socket.on('disconnect', (data) => {
+    for (var i in joueurs) {
+      if (joueurs[i].id == socket.id) {
+        joueurs.splice(i, 1);
+        break;
+      }
+    }
+  })
+  1
+});
 
 httpServer.listen(3000);
 
