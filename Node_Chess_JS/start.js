689625f6d4bb05d14a36121525a87a0863b1601b
antoinelemarie <antoine.lemarie@ynov.com>
2017-05-19 15:35:23 +0200

    lien opé

--- Node_Chess_JS/start.js
+++ Node_Chess_JS/start.js
@@ -1,13 +1,72 @@
-const http = require('http');
-const fs = require('fs');
 const SocketServer = require('socket.io');
 var joueurs = [];
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const FILENAME = __dirname;
 
-const httpServer = http.createServer((request, response) => {
+
+var server = http.createServer();
+
+
+const httpServer = server.on('request', function(request, response) {
   console.log('REQUEST');
-  fs.readFile(__dirname + '/index.html', (err, data) => {
-    response.end(data);
-  });
+  //Recupération de l'url de la page pour connaître le lien direct.
+  var page = url.parse(request.url)
+    .pathname;
+
+  console.log(page);
+
+  //On récupére l'extention si elle existe
+  var ext = path.extname(page);
+
+  //si il n'y a pas d'extantion et que le nom de la page et / on affiche index.html et sinon on rajoute .html au nom de la page et si elle existe on l'affiche sinon on affiche la page d'erreur
+  if (ext == "") {
+    if (page == "/") {
+      page = "/index.html";
+      console.log(page);
+      var lien = fs.createReadStream(FILENAME + page);
+      lien.pipe(response);
+    } else {
+      page = page + ".html";
+      console.log(page);
+      var test = fs.existsSync(FILENAME + page);
+      console.log("Existe : " + test);
+      if (test == true) {
+        var lien = fs.createReadStream(FILENAME + page);
+        lien.pipe(response);
+      } else {
+        var lien = fs.createReadStream("./pages/erreur404.html");
+        lien.pipe(response);
+      }
+    }
+
+  }
+  //Si l'extention et egale à .html alors on affiche la page si elle existe au cas contraire on affiche la page d'erreur
+  else if (ext == ".html") {
+    var test = fs.existsSync(FILENAME + page);
+    console.log("Existe : " + test);
+    if (test == true) {
+      var lien = fs.createReadStream(FILENAME + page);
+      lien.pipe(response);
+    } else {
+      var lien = fs.createReadStream("./pages/erreur404.html");
+      lien.pipe(response);
+    }
+  }
+  //On test si c'est une image et on l'affiche
+  else if (ext == ".jpg" || ext == ".png" || ext == ".gif") {
+    var lien = fs.createReadStream(FILENAME + page);
+    lien.pipe(response);
+  } else {
+    console.log("erreur");
+    var lien = fs.createReadStream("./pages/erreur404.html");
+    lien.pipe(response);
+  }
+
+  console.log("");
+
 });
 
 const io = new SocketServer(httpServer);
