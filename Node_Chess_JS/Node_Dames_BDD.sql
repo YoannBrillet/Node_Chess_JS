731a93971d72a48de42ccf3acf41c98fafda8d6a
antoinelemarie <antoine.lemarie@ynov.com>
2017-05-19 15:47:03 +0200

    arrêt des conneries

--- Node_Chess_JS/Node_Dames_BDD.sql
+++ Node_Chess_JS/Node_Dames_BDD.sql
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -14,11 +14,12 @@ USE Node_Dames_BDD;
 CREATE TABLE IF NOT EXISTS joueurs(
 'id' INT(3) NOT NULL AUTO_INCREMENT,
 'pseudo' VARCHAR(25) NOT NULL,
+'password' VARCHAR(25) NOT NULL,
 'score' INT(9),
 PRIMARY KEY ('id')
 )ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
-INSERT INTO 'joueurs' ('id', 'pseudo', 'score') VALUES 
+INSERT INTO 'joueurs' ('id', 'pseudo', 'score') VALUES
 (1, 'God', 999999999),
 (2, 'Diblux', 0),
 (3, 'Couruss', 0),
